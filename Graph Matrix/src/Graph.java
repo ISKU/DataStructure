99a47bff9a298d3363efecdeee7cbf1c1dc7dd53
Kim Min-Ho <minho1a@hanmail.net>
2016-09-18 20:01:31 +0900

    Build graph with matrix

---
+++ Graph Matrix/src/Graph.java
@@ -0,0 +1,64 @@
+import java.util.Stack;
+
+public class Graph {
+
+	int size;
+	String[] vertices;
+	boolean[][] a;
+
+	public Graph(String[] args) {
+		size = args.length;
+		vertices = new String[size];
+		System.arraycopy(args, 0, vertices, 0, size);
+		a = new boolean[size][size];
+	}
+
+	public void add(String v, String w) {
+		int i = index(v), j = index(w);
+		a[i][j] = a[j][i] = true;
+	}
+
+	public String toString() {
+		if (size == 0)
+			return "{}";
+		StringBuffer buf = new StringBuffer("{" + vertex(0));
+		for (int i = 1; i < size; i++)
+			buf.append(", " + vertex(i));
+		return buf + "}";
+	}
+
+	private int index(String v) {
+		for (int i = 0; i < size; i++)
+			if (vertices[i].equals(v))
+				return i;
+		return a.length;
+	}
+
+	private String vertex(int i) {
+		StringBuffer buf = new StringBuffer(vertices[i] + ":");
+		for (int j = 0; j < size; j++)
+			if (a[i][j])
+				buf.append(vertices[j]);
+		return buf + "";
+	}
+
+	public String dfs(String firstVertex) {
+		Stack<String> stack = new Stack<String>();
+		boolean[] mark = new boolean[size];
+		StringBuffer buf = new StringBuffer("{");
+		int x;
+
+		mark[index(firstVertex)] = true;
+		stack.push(firstVertex);
+		while (!stack.isEmpty()) {
+			x = index(stack.pop());
+			buf.append(vertices[x] + "->");
+			for (int j = 0; j < size; j++)
+				if (a[x][j] && !mark[j]) {
+					mark[j] = true;
+					stack.push(vertices[j]);
+				}
+		}
+		return buf + "}";
+	}
+}
